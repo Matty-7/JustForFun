a0ae9dbd5cd49ce84cf314cc0967c15112f91212
Jingheng <jh730@duke.edu>
2023-07-09 20:22:13 +0800

    Update

--- minesweeper.py
+++ minesweeper.py
@@ -2,21 +2,6 @@ import random
 
 # Step 1: Initialize the game / minefield
 minefield = [[0 for _ in range(10)] for _ in range(10)]
-mines = random.sample(range(100), 20)
-for mine in mines:
-    row = mine // 10
-    col = mine % 10
-    minefield[row][col] = 1
-
-# Calculate the numbers that represent the number of mines around a cell
-numbers = [[0 for _ in range(10)] for _ in range(10)]
-for i in range(10):
-    for j in range(10):
-        for x in [-1, 0, 1]:
-            for y in [-1, 0, 1]:
-                if 0 <= i + x < 10 and 0 <= j + y < 10 and minefield[i + x][j + y] == 1:
-                    numbers[i][j] += 1
-
 # The game board that the player sees
 game_board = [["_" for _ in range(10)] for _ in range(10)]
 
@@ -33,6 +18,26 @@ while not game_over:
     move_row = int(input("请输入你的移动行（1-10）：")) - 1  # subtract 1 to convert to 0-indexing
     move_col = int(input("请输入你的移动列（1-10）：")) - 1  # subtract 1 to convert to 0-indexing
     
+    # Place the mines after the first move
+    if moves_made == 0 and action.lower() == "c":
+        # Make sure the first move is not a mine
+        safe_cells = list(range(100))
+        safe_cells.remove(10 * move_row + move_col)
+        mines = random.sample(safe_cells, 20)
+        for mine in mines:
+            row = mine // 10
+            col = mine % 10
+            minefield[row][col] = 1
+
+        # Calculate the numbers that represent the number of mines around a cell
+        numbers = [[0 for _ in range(10)] for _ in range(10)]
+        for i in range(10):
+            for j in range(10):
+                for x in [-1, 0, 1]:
+                    for y in [-1, 0, 1]:
+                        if 0 <= i + x < 10 and 0 <= j + y < 10 and minefield[i + x][j + y] == 1:
+                            numbers[i][j] += 1
+
     # Perform the chosen action
     if action.lower() == "c":
         # Check if the move is on a mine
